eda2b19bdf39328c7e5305330725fd03775027d8
Luisguilhermeferreira <105927863+Luisguilhermeferreira@users.noreply.github.com>
2022-05-20 10:20:24 -0300

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,34 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box  = createSprite(200,200,30,30);
+  
+}
+
+function draw() 
+{
+  
+  background(30);
+  
+  if(keyDown("RIGHT")){
+  box.x = box.x +2;
+  }
+  if(keyDown("LEFT")){
+  box.x = box.x -2;
+  }
+  if(keyDown("up")){
+  box.y = box.y -2
+  }
+  if(keyDown("down")){
+  box.y = box.y +2
+  }
+    drawSprites();
+  
+
+
+}
+
+
+
+
